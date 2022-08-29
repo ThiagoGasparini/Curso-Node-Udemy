cf88a53dd92eeaddecb3ffacb31f88b358253ca4
Thiago Gasparini <thiago_engprod@outlook.com>
2022-08-29 16:54:40 -0300

    comandos iniciais de node.js

---
+++ inicio/calculadora.js
@@ -0,0 +1,22 @@
+const soma = (a, b) => {
+  return a + b;
+};
+
+const sub = (a, b) => {
+  return a - b;
+};
+
+const div = (a, b) => {
+  return a / b;
+};
+
+const mult = (a, b) => {
+  return a * b;
+};
+
+module.exports = {
+  soma,
+  sub,
+  div,
+  mult
+};
