a54634f45f9ec11a82d70996d006ba3d98aeed86
Thiago Gasparini <thiago_engprod@outlook.com>
2022-08-29 18:13:10 -0300

    rotas e parametros

---
+++ bloco-3/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const app = express();
+const PORT = '3000';
+
+app.get("/",function(req,res){
+  res.status(200).send("<h1>Bem vindo ao guia do programador!!!</h1>");
+});
+
+app.get("/blog/:artigo?", (req, res) => {
+  const { artigo } = req.params;
+  if(artigo) {
+    return res.status(200).send(`<p>Bem vindo ao meu blog do artigo ${artigo}</p>`)
+  }
+  return res.status(200).send(`<p>Bem vindo ao meu blog</p>`)
+  
+})
+
+app.get("/canal/youtube", (req, res) => {
+  res.status(200).send("<p>Bem vindo ao meu canal!!!</p>")
+})
+
+app.get("/ola/:nome/:empresa", (req, res) => {
+  const { nome, empresa } = req.params;
+  res.status(200).send(`Funcionário ${nome} trabalhava na empresa: ${empresa}`)
+})
+
+app.listen(PORT, (error) => {
+  if(error) {
+    return console.log("Ocorreu um erro no servidor");
+  } else {
+    return console.log("Servidor iniciado com sucesso");
+  }
+})
+
