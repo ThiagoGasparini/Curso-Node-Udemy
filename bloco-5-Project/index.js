aa43905ea17a911c0d3190448f74a55141c82212
Thiago Gasparini <thiago_engprod@outlook.com>
2022-08-31 16:12:45 -0300

    estruturas de repetição EJS

--- bloco-5-Project/index.js
+++ bloco-5-Project/index.js
@@ -7,12 +7,18 @@ app.get('/:nome/:lang', (req, res) => {
   // let nome  = 'Thiago Gasparini';
   // let lang = 'JavaScript';
   let exibeMsg = true;
+  let produtos = [
+    {nome: 'doritos', preco: 3.14},
+    {nome: 'coca-cola', preco: 5.20},
+    {nome: 'Leite', preco: 2.50},
+  ];
   res.render('index', {
     nome: req.params.nome,
     lang: req.params.lang,
     empresa: 'Guia do Programador',
     inscritos: 8000,
     msg: exibeMsg,
+    produtos: produtos,
   })
 });
 
