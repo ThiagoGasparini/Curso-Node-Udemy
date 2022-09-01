5471394446ab717c80e683240e46d9fea43d1bd3
Thiago Gasparini <thiago_engprod@outlook.com>
2022-09-01 18:28:46 -0300

    bootstrap, partials e formulario

--- bloco-5-Project/index.js
+++ bloco-5-Project/index.js
@@ -1,30 +1,24 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/:nome/:lang', (req, res) => {
-  // let nome  = 'Thiago Gasparini';
-  // let lang = 'JavaScript';
-  let exibeMsg = true;
-  let produtos = [
-    {nome: 'doritos', preco: 3.14},
-    {nome: 'coca-cola', preco: 5.20},
-    {nome: 'Leite', preco: 2.50},
-  ];
-  res.render('index', {
-    nome: req.params.nome,
-    lang: req.params.lang,
-    empresa: 'Guia do Programador',
-    inscritos: 8000,
-    msg: exibeMsg,
-    produtos: produtos,
-  })
+app.get('/', (_req, res) => {
+  res.render('index')
 });
 
-app.get('/home', (_req, res) => {
-  res.render('home')
+app.get('/perguntar', (_req, res) => {
+  res.render('perguntar');
+});
+
+app.post('/salvarpergunta', (req, res) => {
+  let titulo = req.body.titulo
+  let descricao = req.body.descricao
+  res.send(`formulário recebido! titulo: ${titulo}, descricao: ${descricao}`);
 });
 
 app.listen(8080, () => {
