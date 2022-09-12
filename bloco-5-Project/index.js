cded3ffd165e49ca2b7ef14d7d9f7f49b697070e
Thiago Gasparini <thiago_engprod@outlook.com>
2022-09-12 16:49:48 -0300

    finalizando projeto exibindo as respostas

--- bloco-5-Project/index.js
+++ bloco-5-Project/index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./database/database');
 const Pergunta = require('./database/Pergunta');
+const Resposta = require('./database/Resposta');
 
 connection
   .authenticate()
@@ -38,8 +39,16 @@ app.get('/pergunta/:id', (req, res) => {
     where: {id: id}
   }).then((pergunta) => {
     if (pergunta !== undefined) {
-      res.render('pergunta', {
-        pergunta: pergunta
+      Resposta.findAll({
+        where: {perguntaId: pergunta.id},
+        order: [
+          ['id', 'DESC']
+        ]
+      }).then((respostas) => {
+        res.render('pergunta', {
+          pergunta: pergunta,
+          respostas: respostas
+        })
       })
     } else {
       res.redirect('/')
@@ -56,6 +65,17 @@ app.post('/salvarpergunta', (req, res) => {
   }).then(() => {
     res.redirect('/');
   })
+});
+
+app.post('/responder', (req, res) => {
+  let corpo = req.body.corpo;
+  let perguntaId = req.body.pergunta;
+  Resposta.create({
+    corpo: corpo,
+    perguntaId: perguntaId
+  }).then(() => {
+    res.redirect('/pergunta/' + perguntaId);
+  });
 });
 
 
