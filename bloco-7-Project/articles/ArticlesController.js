f7bee494061fcc8271c5c65302542b3ee586edfc
Thiago Gasparini <thiago_engprod@outlook.com>
2022-10-19 17:03:41 -0300

    salva artigos no DB

--- bloco-7-Project/articles/ArticlesController.js
+++ bloco-7-Project/articles/ArticlesController.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Category = require('../categories/Category');
+const Article = require('./Article');
+const slugify = require('slugify');
 
-router.get('/articles', (_req, res) => {
+router.get('/admin/articles', (req, res) => {
   res.send('rota de artigos');
 });
 
@@ -11,6 +13,21 @@ router.get('/admin/articles/new', (_req, res) => {
     res.render('admin/articles/new', {
       categories: categories,
     });
+  });
+});
+
+router.post('/articles/save', (req, res) => {
+  let title = req.body.title;
+  let body = req.body.body;
+  let category = req.body.category;
+
+  Article.create({
+    title: title,
+    slug: slugify(title),
+    body: body,
+    categoryId: category,
+  }).then(() => {
+    res.redirect('/admin/articles');
   });
 });
 
