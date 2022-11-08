1e2c8d282d211c22b7f6d2a4d00be3122e486d1e
Thiago Gasparini <thiago_engprod@outlook.com>
2022-11-08 14:04:10 -0300

    add edição de artigos

--- bloco-7-Project/articles/ArticlesController.js
+++ bloco-7-Project/articles/ArticlesController.js
@@ -58,4 +58,42 @@ router.post('/articles/delete', (req, res) => {
   }
 });
 
+router.get('/admin/articles/edit/:id', (req, res) => {
+  let id = req.params.id
+  if (isNaN(id)) {
+    res.redirect('/admin/articles')
+  }
+  Article.findByPk(id).then(article => {
+    if(article !== undefined) {
+      Category.findAll().then(categories => {
+        res.render('admin/articles/edit', {
+          categories: categories,
+          article: article
+        })
+      })
+    } else {
+      res.redirect('/admin/articles');
+    }
+  }).catch(error => {
+    res.redirect('/admin/articles');
+  });
+});
+
+router.post('/articles/update', (req, res) => {
+  let id = req.body.id;
+  let title = req.body.title;
+  let body = req.body.body;
+  let category = req.body.category;
+
+  Article.update({ title: title, slug: slugify(title), body: body, categoryId: category }, {
+    where: {
+      id: id,
+    }
+  }).then(() => {
+    res.redirect('/admin/articles');
+  }).catch(err => {
+    res.redirect('/');
+  })
+});
+
 module.exports = router;
